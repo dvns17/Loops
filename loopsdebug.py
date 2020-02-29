f387cfc3e16acb2708efbad201ef367e94a19799
Danica Washington <55327173+dvns17@users.noreply.github.com>
2020-02-29 14:43:32 -0600

    Create loopsdebug.py

---
+++ loopsdebug.py
@@ -0,0 +1,108 @@
+'''
+Created on Feb 29, 2020
+
+@author: ITAUser
+'''
+'''
+START HERE
+'''
+
+#1)
+#Broken:
+#i = 6
+#WHILE i != 2:
+#    print(i)
+#    i -= 1
+
+i = 6
+while i !=2:
+    print(i)
+    1 -=1
+    
+#2)
+#Broken:
+#i = 7
+#whiLe i < 10:
+#    if i == 9:
+#        break
+#    print(i)
+#    i += 1
+    
+1 = 7
+while i < 10:
+    print(i)
+    i +=1
+    if 1 == 9:
+        break
+
+#3)
+#Broken:
+#i = 7
+#while i > 10
+#    if i == 9:
+#        break
+#    print(i)
+#    i += 1
+1 = 7
+while 1 < 10:
+    print (i) 
+    i+= 1
+    if i == 9:
+        break
+
+#4)
+#Broken:
+#i = 11
+#while i > 10:
+#    print(i)
+#    i += 1
+
+1 = 11
+while 1 < 10:
+    print(i)
+    i-= 1
+
+'''
+For the next section, the directions are the same but we will be working with
+for loops
+EXAMPLE:
+'''
+#EX)
+#foR i in "Dylan":
+#    print i
+#
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+#1)
+#for i in "Dylan":
+#    print i
+#
+Dylan =["D","y","l","a","n"]
+for i in Dylan:
+    print(i)
+#2)
+#for j in "Dylan":
+#    print i
+Dylan =["D","y","l","a","n"]
+for j in Dylan:
+    print(j)
+
+#3)
+#for i in range(1, 5)
+#    print i 
+#ANSWER:
+for i range(1, 5):
+    print(i)
+#4)
+##3)
+#for i in range(1, 5:
+#    print i 
+#ANSWER:
+for i in range(1, 5):
+    print(i)
